1f5822f6958fa2b272a2d96f39476097bba39894
Alexandru Dragu <alex@getstudio.co.uk>
2018-03-10 15:12:14 +0000

    Modified sed file

--- scripts/memory_limit.sh
+++ scripts/memory_limit.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # Changing Memory Limit every time you run Vagrant UP
 echo "Changing memory limit to 1024M"
-sed -i "s#php_admin_value[memory_limit] = 256M#php_admin_value[memory_limit] = 1024M#g" /opt/etc/fpm/fpm-pool-common.conf
-sed -i "s#memory_limit = 256M#memory_limit = 1024M#g" /opt/php70/etc/php70.ini
+sed -i 's/256M/1024M/g' /opt/etc/fpm/fpm-pool-common.conf
 exit
